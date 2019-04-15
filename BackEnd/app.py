f3ab5f8143e987d0ef395c72a7b556bc668e77ab
CS70 Orca <qhirsohn@cs.hmc.edu>
2019-04-14 20:00:00 -0700

    cleaning up master

---
+++ BackEnd/app.py
@@ -0,0 +1,26 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def hello_route():
+    return "Hello, world!"
+
+
+@app.route("/about")
+def about_route():
+    return flask.send_from_directory(".", "hello.html")
+
+
+@app.route("/api/v1/greeting", methods=["POST"])
+def greeting_api():
+    request = flask.request.get_json(silent=True)
+    if isinstance(request, dict):
+        response = {
+            "greeting": "Hello, " + request.get("name", "friend") + "!"
+        }
+    else:
+        response = {
+            "error": "Bad input"
+        }
+    return flask.jsonify(response)
